37b510935dc4de1057c013c542bb779122a1decb
Mariana Pacuraru <pacuraru.mariana@yahoo.com>
2025-05-22 16:02:42 +0100

    Added button to create a new superpower and restricted delete option to user-created superpowers

--- app/models/superpower.rb
+++ app/models/superpower.rb
@@ -1,6 +1,7 @@
 class Superpower < ApplicationRecord
   belongs_to :user
-
+  has_many :bookings, dependent: :destroy
+  has_one_attached :image
   validates :name, presence: true
   validates :price, presence: true
 end
